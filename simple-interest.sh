b1bee0bb546fbb8131f0c459f9554b7915aa5897
wael ABDALLAH <129751261+waelABDALLAH1@users.noreply.github.com>
2023-10-21 18:52:57 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # wael abdallah
+   # waelABDALLAH1
    # Input:
    # p, principal amount
    # t, time period in years
